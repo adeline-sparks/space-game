c778bb6b67ba4e2a3c96541f00a7b0a680aaf7b7
Adeline Thompson <adeline.sprks@gmail.com>
2022-01-22 12:20:09 -0800

    Server serves the wasm files

--- crates/space_game_server/src/main.rs
+++ crates/space_game_server/src/main.rs
@@ -1,12 +1,30 @@
+use std::path::Path;
+
 use warp::Filter;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap()]
+struct Args {
+    #[clap(long)]
+    space_game_pkg: String,
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let args = Args::parse();
 
-    warp::serve(hello)
+    assert!(Path::new(&args.space_game_pkg).is_dir());
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = 
+        warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+    let space_game_pkg = 
+        warp::fs::dir(args.space_game_pkg);
+    let filters = 
+        hello.or(space_game_pkg);
+    warp::serve(filters)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
