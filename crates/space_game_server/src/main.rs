1e8ad0fd68142c7cd1b83072cb6d031349aadfc6
Adeline Thompson <adeline.sprks@gmail.com>
2022-01-22 12:47:35 -0800

    Added address argument

--- crates/space_game_server/src/main.rs
+++ crates/space_game_server/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, net::SocketAddr};
 
 use warp::Filter;
 use clap::Parser;
@@ -8,12 +8,14 @@ use clap::Parser;
 struct Args {
     #[clap(long)]
     space_game_pkg: String,
+
+    #[clap(long, default_value="127.0.0.1:3030")]
+    addr: SocketAddr,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-
     assert!(Path::new(&args.space_game_pkg).is_dir());
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
@@ -25,6 +27,6 @@ async fn main() {
     let filters = 
         hello.or(space_game_pkg);
     warp::serve(filters)
-        .run(([127, 0, 0, 1], 3030))
+        .run(args.addr)
         .await;
 }
