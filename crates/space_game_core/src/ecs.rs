2e5e94c2d2ef833a40693edfec5a32e5885c9d0f
Adeline Sparks <adeline.sprks@gmail.com>
2022-01-25 13:48:35 -0800

    ECS passes a test!

--- crates/space_game_core/src/ecs.rs
+++ crates/space_game_core/src/ecs.rs
@@ -1,89 +1,42 @@
-use std::{any::{Any, TypeId}, collections::HashMap, ops::Deref};
+use std::{any::{Any, TypeId}, collections::{HashMap, HashSet}};
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct SystemId(TypeId);
 
 impl SystemId {
     pub fn of<S: System>() -> Self { SystemId(TypeId::of::<S>()) }
 }
 
-impl From<&dyn ErasedSystem> for SystemId {
-    fn from(sys: &dyn ErasedSystem) -> Self {
+impl From<&dyn AnySystem> for SystemId {
+    fn from(sys: &dyn AnySystem) -> Self {
         SystemId(sys.as_any().type_id())
     }
 }
 
-pub trait System : Sized + 'static {
-    type Inputs : for <'a> SystemInputs<'a>;
-
-    fn update(&mut self, inputs: Self::Inputs);
+pub trait System : 'static {
+    fn dependencies(&self) -> Vec<Dependency>;
+    fn update(&mut self, systems: &SystemMap);
 }
 
-pub trait SystemInputs<'a> {
-    fn from(systems: &'a SystemMap) -> Self;
-
-    fn write_dependencies(out: &mut Vec<Dependency>);
-}
-
+#[derive(Clone, Debug)]
 pub enum Dependency {
     Read(SystemId),
     ReadDelay(SystemId),
 }
 
-impl<'a, S: System> SystemInputs<'a> for &'a S {
-    fn from(systems: &'a SystemMap) -> Self {
-        systems.get::<S>()
-    }
-
-    fn write_dependencies(out: &mut Vec<Dependency>) {
-        out.push(Dependency::Read(SystemId::of::<S>()));
-    }
-}
-
-struct Delay<S>(S);
-
-impl<'a, S: System> SystemInputs<'a> for Delay<&'a S> {
-    fn from(systems: &'a SystemMap) -> Self {
-        Delay(systems.get::<S>())
-    }
-
-    fn write_dependencies(out: &mut Vec<Dependency>) {
-        out.push(Dependency::ReadDelay(SystemId::of::<S>()));
-    }
-}
-
-impl<'a, A: SystemInputs<'a>> SystemInputs<'a> for (A,) {
-    fn from(systems: &'a SystemMap) -> Self {
-        (A::from(systems),)
-    }
-
-    fn write_dependencies(out: &mut Vec<Dependency>) {
-        A::write_dependencies(out);
-    }
-}
-
-impl<'a, A: SystemInputs<'a>, B: SystemInputs<'a>> SystemInputs<'a> for (A, B) {
-    fn from(systems: &'a SystemMap) -> Self {
-        (A::from(systems), B::from(systems))
-    }
-
-    fn write_dependencies(out: &mut Vec<Dependency>) {
-        A::write_dependencies(out);
-        B::write_dependencies(out);
-    }
-}
-
 #[derive(Default)]
 pub struct SystemMap {
-    systems: HashMap<SystemId, Option<Box<dyn ErasedSystem>>>,
+    systems: HashMap<SystemId, Option<Box<dyn AnySystem>>>,
 }
 
-trait ErasedSystem {
-    fn update_erased(&mut self, systems: &SystemMap);
-    fn get_dependencies_erased(&self) -> Vec<Dependency>;
-
+trait AnySystem : System {
     fn as_any(&self) -> &dyn Any;
     fn as_any_box(self: Box<Self>) -> Box<dyn Any>;
+}
+
+impl<T: System + Sized> AnySystem for T {
+    fn as_any(&self) -> &dyn Any { self }
+    fn as_any_box(self: Box<Self>) -> Box<dyn Any> { self }
 }
 
 impl SystemMap {
@@ -113,22 +66,14 @@ impl SystemMap {
             .unwrap()
     }
 
-    fn get_erased(&self, id: SystemId) -> &dyn ErasedSystem {
-        self.systems.get(&id)
-            .expect("Can't get system that was not inserted")
-            .as_ref()
-            .expect("Can't get system that was taken")
-            .deref()
-    }
-
-    fn take_erased(&mut self, id: SystemId) -> Box<dyn ErasedSystem> { 
+    fn take_any(&mut self, id: SystemId) -> Box<dyn AnySystem> { 
         self.systems.get_mut(&id)
             .expect("Can't take system that was not inserted")
             .take()
             .expect("Can't take system that was already taken")
     }
 
-    fn untake_erased(&mut self, sys: Box<dyn ErasedSystem>) { 
+    fn untake_any(&mut self, sys: Box<dyn AnySystem>) { 
         let id = SystemId::from(sys.as_ref());
         let sys_opt = self.systems.get_mut(&id)
             .expect("Can't untake system that was never inserted");
@@ -138,31 +83,54 @@ impl SystemMap {
         *sys_opt = Some(sys);
     }
 
-    pub fn topological_order(&self) -> Vec<SystemId> { 
+    pub fn topological_order(&self) -> Result<Vec<SystemId>, ()> { 
         let mut dep_map = HashMap::<SystemId, Vec<SystemId>>::new();
         for sys in self.systems.values() {
-            let sys = sys.as_ref().expect("Can't compute topological_order with taken System(s)");
+            let sys = sys.as_deref().expect("Can't compute topological_order with taken System(s)");
+            let sys_id = SystemId::from(sys);
+            for dep in sys.dependencies() {
+                match dep {
+                    Dependency::Read(dep_id) => {
+                        dep_map.entry(sys_id).or_default().push(dep_id);
+                    }
+                    Dependency::ReadDelay(dep_id) => {
+                        dep_map.entry(dep_id).or_default().push(sys_id);
+                    }
+                }
+            }
+        }
+        let dep_map = dep_map;
+
+        fn visit(id: SystemId, dep_map: &HashMap<SystemId, Vec<SystemId>>, unvisited: &mut HashSet<SystemId>, pending: &mut HashSet<SystemId>, result: &mut Vec<SystemId>) -> Result<(), ()> {
+            if !unvisited.remove(&id) {
+                return Ok(());
+            }
+
+            if !pending.insert(id) {
+                return Err(());
+            }
+
+            if let Some(children) = dep_map.get(&id) {
+                for &child in children {
+                    visit(child, dep_map, unvisited, pending, result)?;
+                }
+            }
+
+            pending.remove(&id);
+            result.push(id);
+
+            Ok(())
         }
 
-
+        let mut unvisited = self.systems.keys().cloned().collect::<HashSet<_>>();
+        let mut pending: HashSet<SystemId> = HashSet::new();
         let mut result = Vec::new();
-        todo!()
-    }
-}
+        while let Some(&id) = unvisited.iter().next() {
+            visit(id, &dep_map, &mut unvisited, &mut pending, &mut result)?;
+        }
 
-impl<I: for<'a> SystemInputs<'a>, S: System<Inputs=I>> ErasedSystem for S {
-    fn update_erased(&mut self, systems: &SystemMap) {
-        self.update(I::from(systems));
+        Ok(result)
     }
-
-    fn get_dependencies_erased(&self) -> Vec<Dependency> {
-        let mut result = Vec::new();
-        I::write_dependencies(&mut result);
-        result
-    }
-
-    fn as_any(&self) -> &dyn Any { self }
-    fn as_any_box(self: Box<Self>) -> Box<dyn Any> { self }
 }
 
 #[derive(Default)]
@@ -185,13 +153,57 @@ impl World {
 
     pub fn update(&mut self) {
         let order = self.topological_order
-            .get_or_insert_with(|| self.systems.topological_order())
+            .get_or_insert_with(|| self.systems.topological_order().unwrap())
             .as_slice();
 
         for &id in order {
-            let mut sys = self.systems.take_erased(id);
-            sys.update_erased(&self.systems);
-            self.systems.untake_erased(sys);
+            let mut sys = self.systems.take_any(id);
+            sys.update(&self.systems);
+            self.systems.untake_any(sys);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_simple() {
+        struct SysA(usize);
+        struct SysB(usize);
+        struct SysC(usize);
+
+        impl System for SysA {
+            fn dependencies(&self) -> Vec<Dependency> { vec![] }
+            fn update(&mut self, _systems: &SystemMap) { self.0 += 1; }
+        }
+
+        impl System for SysB {
+            fn dependencies(&self) -> Vec<Dependency> { vec![] }
+            fn update(&mut self, _systems: &SystemMap) { self.0 += 2; }
+        }
+
+        impl System for SysC {
+            fn dependencies(&self) -> Vec<Dependency> { 
+                vec![
+                    Dependency::Read(SystemId::of::<SysA>()), 
+                    Dependency::Read(SystemId::of::<SysB>()), 
+                ]
+            }
+            fn update(&mut self, systems: &SystemMap) { 
+                self.0 = systems.get::<SysA>().0 + systems.get::<SysB>().0;
+            }
+        }
+
+        let mut world = World::new();
+        world.systems_mut().insert(SysA(0));
+        world.systems_mut().insert(SysB(0));
+        world.systems_mut().insert(SysC(0));
+
+        for i in 0..10 {
+            world.update();
+            assert_eq!(world.systems().get::<SysC>().0, 3 * (i + 1));
         }
     }
 }
