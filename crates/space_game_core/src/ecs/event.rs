e61c0b1e5f194efe086600cff52ef0cb35f39169
Adeline Thompson <adeline.sprks@gmail.com>
2022-01-26 21:16:42 -0800

    Split into child modules

---
+++ crates/space_game_core/src/ecs/event.rs
@@ -0,0 +1,89 @@
+use std::{any::{TypeId, Any}, collections::{VecDeque, HashMap}, cell::RefCell};
+
+use super::{Dependency, SystemInputs, SystemMap};
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct EventId(TypeId);
+
+impl EventId {
+    pub fn of<E: Event>() -> Self { Self(TypeId::of::<E>()) }
+}
+
+pub trait Event: 'static + Any { }
+pub trait AnyEvent: Event {
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<E: Event> AnyEvent for E {
+    fn as_any(&self) -> &dyn Any { self }
+}
+
+pub struct EventQueue<E>(RefCell<VecDeque<E>>);
+
+impl<E> Default for EventQueue<E> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+
+impl<E: Event> EventQueue<E> {
+    pub fn push(&self, val: E) {
+        self.0.borrow_mut().push_back(val);
+    }
+
+    pub fn pop(&self) -> Option<E> {
+        self.0.borrow_mut().pop_front()
+    }
+}
+
+impl<'a, E: Event> SystemInputs<'a> for &'a EventQueue<E> {
+    fn write_dependencies(output: &mut Vec<Dependency>) {
+        output.push(Dependency::Emit(EventId::of::<E>()));
+    }
+
+    fn assemble(_systems: &'a SystemMap, events: &'a EventQueueMap) -> Self {
+        events.get()
+    }
+}
+
+#[derive(Default)]
+pub struct EventQueueMap {
+    queues: HashMap<EventId, Box<dyn AnyEventQueue>>
+}
+
+pub trait AnyEventQueue {
+    fn len(&self) -> usize;
+    fn pop_any(&self) -> Option<Box<dyn AnyEvent>>;
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<E: Event> AnyEventQueue for EventQueue<E> {
+    fn len(&self) -> usize {
+        self.0.borrow().len()
+    }
+    fn pop_any(&self) -> Option<Box<dyn AnyEvent>> {
+        Some(Box::new(self.0.borrow_mut().pop_front()?))
+    }
+
+    fn as_any(&self) -> &dyn Any { self }
+}
+
+impl EventQueueMap {
+    pub fn register<E: Event>(&mut self) {
+        self.queues.insert(
+            EventId::of::<E>(), 
+            Box::new(EventQueue::<E>::default()));
+    }
+
+    pub fn get<E: Event>(&self) -> &EventQueue<E> {
+        self.queues[&EventId::of::<E>()]
+            .as_any()
+            .downcast_ref::<EventQueue<E>>()
+            .unwrap()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=&dyn AnyEventQueue> {
+        self.queues.values().map(|v| v.as_ref())
+    }
+}
---
+++ crates/space_game_core/src/ecs/system.rs
@@ -0,0 +1,224 @@
+use std::{any::{TypeId, Any}, collections::{HashMap, HashSet}, ops::Deref};
+
+use super::{AnyEvent, EventQueueMap, EventId};
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct SystemId(TypeId);
+
+impl SystemId {
+    pub fn of<'a, S: System<'a>>() -> Self { Self(TypeId::of::<S>()) }
+}
+
+pub trait System<'a> : 'static {
+    type Inputs: SystemInputs<'a>;
+
+    fn update(&mut self, inputs: Self::Inputs);
+    fn on_event(&mut self, _event: &dyn AnyEvent) { }
+}
+
+pub trait SystemInputs<'a> {
+    fn write_dependencies(output: &mut Vec<Dependency>);
+    fn assemble(systems: &'a SystemMap, events: &'a EventQueueMap) -> Self;
+}
+
+#[derive(Clone, Debug)]
+pub enum Dependency {
+    Read(SystemId),
+    ReadDelay(SystemId),
+    Emit(EventId),
+}
+
+impl<'a, S: System<'a>> SystemInputs<'a> for &'a S {
+    fn write_dependencies(output: &mut Vec<Dependency>) {
+        output.push(Dependency::Read(SystemId::of::<S>()));
+    }
+
+    fn assemble(systems: &'a SystemMap, _events: &'a EventQueueMap) -> Self {
+        systems.get::<S>()
+    }
+}
+
+impl<'a> SystemInputs<'a> for () {
+    fn write_dependencies(_output: &mut Vec<Dependency>) { }
+    fn assemble(_systems: &'a SystemMap, _events: &'a EventQueueMap) -> Self { () }
+}
+
+impl<'a, A: SystemInputs<'a>, B: SystemInputs<'a>> SystemInputs<'a> for (A, B) {
+    fn write_dependencies(output: &mut Vec<Dependency>) {
+        A::write_dependencies(output);
+        B::write_dependencies(output);
+    }
+
+    fn assemble(systems: &'a SystemMap, events: &'a EventQueueMap) -> Self {
+        (A::assemble(systems, events), B::assemble(systems, events))
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Delay<'a, S>(&'a S);
+
+impl<'a, S: System<'a>> SystemInputs<'a> for Delay<'a, S> {
+    fn write_dependencies(output: &mut Vec<Dependency>) {
+        output.push(Dependency::ReadDelay(SystemId::of::<S>()));
+    }
+
+    fn assemble(systems: &'a SystemMap, _events: &'a EventQueueMap) -> Self {
+        Delay(systems.get::<S>())
+    }
+}
+
+impl<'a, S> Deref for Delay<'a, S> {
+    type Target = S;
+
+    fn deref(&self) -> &Self::Target { self.0 }
+}
+
+#[derive(Default)]
+pub struct SystemMap {
+    systems: HashMap<SystemId, Option<Box<DynAnySystem>>>,
+}
+
+pub trait AnySystem<'a> {
+    fn dependencies(&self) -> Vec<Dependency>;
+    fn any_update(&mut self, systems: &'a SystemMap, events: &'a mut EventQueueMap);
+    fn any_event(&mut self, ev: &dyn AnyEvent);
+
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+    fn as_any_box(self: Box<Self>) -> Box<dyn Any>;
+}
+
+type DynAnySystem = dyn for<'a> AnySystem<'a>;
+
+impl<'a, S: System<'a>> AnySystem<'a> for S {
+    fn dependencies(&self) -> Vec<Dependency> {
+        let mut result = Vec::new();
+        S::Inputs::write_dependencies(&mut result);
+        result
+    }
+
+    fn any_update(&mut self, systems: &'a SystemMap, events: &'a mut EventQueueMap) {
+        self.update(S::Inputs::assemble(systems, events));
+    }
+
+    fn any_event(&mut self, ev: &dyn AnyEvent) {
+        self.on_event(ev);
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn as_any_box(self: Box<Self>) -> Box<dyn Any> {
+        self
+    }    
+}
+
+impl From<&DynAnySystem> for SystemId {
+    fn from(sys: &DynAnySystem) -> Self {
+        SystemId(sys.as_any().type_id())
+    }
+}
+
+impl SystemMap {
+    pub fn new() -> Self { Default::default() }
+
+    pub fn insert<S: for<'a> System<'a>>(&mut self, sys: S) {
+        if self.systems.insert(SystemId::of::<S>(), Some(Box::new(sys))).is_some() {
+            panic!("Can't insert duplicate system");
+        }
+    }
+    pub fn remove<'a, S: System<'a>>(&mut self) -> S {
+        *self.systems.remove(&SystemId::of::<S>())
+            .expect("Can't remove system that was not inserted")
+            .expect("Can't remove system that was taken")
+            .as_any_box()
+            .downcast()
+            .unwrap()
+    }
+
+    pub fn get<'a, S: System<'a>>(&self) -> &S { 
+        self.systems.get(&SystemId::of::<S>())
+            .expect("Can't get system that was not inserted")
+            .as_ref()
+            .expect("Can't get system that was taken")
+            .as_any()
+            .downcast_ref()
+            .unwrap()
+    }
+
+    pub fn take_any(&mut self, id: SystemId) -> Box<DynAnySystem> { 
+        self.systems.get_mut(&id)
+            .expect("Can't take system that was not inserted")
+            .take()
+            .expect("Can't take system that was already taken")
+    }
+
+    pub fn untake_any(&mut self, sys: Box<DynAnySystem>) { 
+        let id = SystemId::from(sys.as_ref());
+        let sys_opt = self.systems.get_mut(&id)
+            .expect("Can't untake system that was never inserted");
+        if sys_opt.is_some() {
+            panic!("Can't untake system that was never taken");
+        }
+        *sys_opt = Some(sys);
+    }
+
+    pub fn topological_order(&self) -> Result<Vec<SystemId>, ()> { 
+        let mut dep_map = HashMap::<SystemId, Vec<SystemId>>::new();
+        for sys in self.systems.values() {
+            let sys = sys.as_deref().expect("Can't compute topological_order with taken System(s)");
+            let sys_id = SystemId::from(sys);
+            for dep in sys.dependencies() {
+                match dep {
+                    Dependency::Read(dep_id) => {
+                        dep_map.entry(sys_id).or_default().push(dep_id);
+                    }
+                    Dependency::ReadDelay(dep_id) => {
+                        dep_map.entry(dep_id).or_default().push(sys_id);
+                    }
+                    Dependency::Emit(_) => (),
+                }
+            }
+        }
+        let dep_map = dep_map;
+
+        fn visit(id: SystemId, dep_map: &HashMap<SystemId, Vec<SystemId>>, unvisited: &mut HashSet<SystemId>, pending: &mut HashSet<SystemId>, result: &mut Vec<SystemId>) -> Result<(), ()> {
+            if !unvisited.remove(&id) {
+                return Ok(());
+            }
+
+            if !pending.insert(id) {
+                return Err(());
+            }
+
+            if let Some(children) = dep_map.get(&id) {
+                for &child in children {
+                    visit(child, dep_map, unvisited, pending, result)?;
+                }
+            }
+
+            pending.remove(&id);
+            result.push(id);
+
+            Ok(())
+        }
+
+        let mut unvisited = self.systems.keys().cloned().collect::<HashSet<_>>();
+        let mut pending: HashSet<SystemId> = HashSet::new();
+        let mut result = Vec::new();
+        while let Some(&id) = unvisited.iter().next() {
+            visit(id, &dep_map, &mut unvisited, &mut pending, &mut result)?;
+        }
+
+        Ok(result)
+    }
+
+    pub fn iter_systems_mut(&mut self) -> impl Iterator<Item=&mut DynAnySystem> {
+        self.systems.values_mut().map(|slot| slot.as_mut().unwrap().as_mut())
+    }
+}
