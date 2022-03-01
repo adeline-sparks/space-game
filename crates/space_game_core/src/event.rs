0b11c07e669c4984bfc4c7a87a842060cf0378b6
Adeline Thompson <adeline.sprks@gmail.com>
2022-02-28 22:05:44 -0800

    Small cleanups on event

--- crates/space_game_core/src/event.rs
+++ crates/space_game_core/src/event.rs
@@ -7,23 +7,21 @@ pub trait Event: 'static {}
 
 pub type AnyEvent = Box<dyn Any>;
 
-pub struct Dispatcher(HashMap<TypeId, Box<dyn Fn(&World, AnyEvent) -> Vec<AnyEvent>>>);
+type EventHandlerFn = Box<dyn Fn(AnyEvent, &World) -> Vec<AnyEvent>>;
 
-impl Dispatcher {
-    pub fn install_handler<E: Event>(&mut self, f: impl Fn(&World, E) -> Vec<AnyEvent> + 'static) {
-        self.0.insert(
-            TypeId::of::<E>(),
-            Box::new(move |world, ev| f(world, *ev.downcast().unwrap())),
-        );
-    }
+#[derive(Default)]
+pub struct Reactor {
+    event_handlers: HashMap<TypeId, EventHandlerFn>,
+}
 
+impl Reactor {
     pub fn dispatch(&self, world: &World, ev: AnyEvent) {
         let mut queue = VecDeque::new();
         queue.push_back(ev);
 
-        while let Some(ev) = queue.pop_front() {
-            if let Some(handler) = self.0.get(&ev.type_id()) {
-                queue.extend(handler(world, ev).into_iter());
+        while let Some(event) = queue.pop_front() {
+            if let Some(handler) = self.event_handlers.get(&event.type_id()) {
+                queue.extend(handler(event, world).into_iter());
             }
         }
     }
