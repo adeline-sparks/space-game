c2dab224cba7a5d03bb210629cfaa63b7cb1f004
Adeline Thompson <adeline.sprks@gmail.com>
2022-03-06 17:20:04 -0800

    Get rid of initial test

--- crates/space_game_core/src/lib.rs
+++ crates/space_game_core/src/lib.rs
@@ -2,12 +2,3 @@ pub mod orbit;
 
 #[allow(dead_code)]
 pub mod ecs;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
