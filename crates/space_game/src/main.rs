6267957d91665e0eac985566dae5f4fd8e3b3975
Adeline Thompson <adeline.sprks@gmail.com>
2022-04-01 19:28:38 -0700

    Added winit

--- crates/space_game/src/main.rs
+++ crates/space_game/src/main.rs
@@ -1,3 +1,32 @@
-pub fn main() {
-    println!("Hello World");
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use winit::platform::web::WindowExtWebSys;
+
+        let body = web_sys::window().unwrap().document().unwrap().body().unwrap();
+        body.append_child(&window.canvas()).unwrap();
+    }
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                *control_flow = ControlFlow::Exit
+            }
+            _ => (),
+        }
+    });
 }
